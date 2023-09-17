aead99809a6323dbc7649c85e59343073e9b82c5
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 13:09:02 +0000

    NumberAndMath

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,22 @@
+// const score = 400
+// console.log(typeof score);
+
+// const balance = new Number(100)
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// =======Maths==========
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.max(2,4,6,1,10));
+// console.log(Math.min(2,4,6,1,10));
+
+const max =20
+const min =10
+console.log(Math.floor(Math.random() * (max-min) * 100));
