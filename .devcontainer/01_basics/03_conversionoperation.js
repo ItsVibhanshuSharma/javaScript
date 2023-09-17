0567d834d8471c35515c87779d54503f8a3a73e4
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 10:52:10 +0000

    Operations

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -10,10 +10,44 @@
 // 0 => false
 
 
-let number =22
-console.log(number)
+// let number =22
+// console.log(number)
 
-console.log(typeof number);
+// console.log(typeof number);
 
-let conversion = String(number)
-console.log(typeof conversion);
+// let conversion = String(number)
+// console.log(typeof conversion);
+
+
+// ******Operation*******
+
+let value  = 3
+
+let negValue = -value
+// console.log([value , negValue]);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello "
+let str2 ="Vibhanshu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+// console.log( 2+ "1" + 2);
+
+console.log(true);
+
+
+
+
