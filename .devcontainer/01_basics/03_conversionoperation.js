fc0f0de447b3c1e92806e24b59be35196dd490af
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 10:37:31 +0000

    conversion DataTypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+// let score = "33"
+// let isLoggedin = false
+
+// // console.log(typeof score);
+// console.log(isLoggedin);
+//  let conversion = Number(isLoggedin)
+// console.log(typeof  conversion);
+
+// 1 => true
+// 0 => false
+
+
+let number =22
+console.log(number)
+
+console.log(typeof number);
+
+let conversion = String(number)
+console.log(typeof conversion);
