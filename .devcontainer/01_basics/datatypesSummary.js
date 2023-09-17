af4f2029e00680a22ff6cf206ed1b42bda54815c
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 11:23:56 +0000

    datatypeSummary

---
+++ .devcontainer/01_basics/datatypesSummary.js
@@ -0,0 +1,26 @@
+const id = Symbol('123')
+console.log(id);
+
+const studentId = Symbol('123')
+console.log(studentId);
+console.log(id === studentId);
+
+
+const Heroes = ["shaktiman ", "ninja"]
+console.log(Heroes);
+
+const myObj = {
+    name : "Vibhanshu",
+    age : 24,
+    number  : 7669785550,
+}
+
+console.log(myObj);
+
+
+const myfunction = function(){
+    return; // This return statement terminates the function immediately
+    console.log("Hello");
+}
+
+console.log(myfunction);
