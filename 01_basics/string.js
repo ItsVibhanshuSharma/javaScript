fa84e1748270973cde35ee646aed46deebc1ebb0
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 11:58:04 +0000

    Strings

---
+++ 01_basics/string.js
@@ -0,0 +1,27 @@
+// const name = "ishu "
+// const lastName = "Sharma"
+// const FullName = (name + lastName)
+
+
+// console.log(`My name is ${name} and MY last Name is ${lastName}`);
+
+// const name1 = new String("Ishu_Sharma")
+// // console.log(name1);
+// console.log(name1.length);
+// console.log(name1.toUpperCase());
+// console.log(name1.charAt(2));
+// console.log(name1.indexOf('S'));
+
+// const newName = name1.substring(1,6)
+// console.log(newName); 
+
+// const newName2 = name1.slice(-4,2)
+// console.log(newName2);
+
+// const name = "    Ishu    "
+// const name1 = name.trim()
+// console.log(name);
+// console.log(name1);
+
+const Website = "vibhanshusharma%4%1.com"
+console.log(Website.replace('%','-'));
