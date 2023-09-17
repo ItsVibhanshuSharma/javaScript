33afea72b0c2c4d0dd4aaa6fb7b92634df0632ac
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-17 10:07:10 +0000

    Variable s and DataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1445533
+let accountEmail = 'vibhanshusharma1811@gmail.com'
+var accountPassword = "123456"
+accountCity ="Jaipur"
+
+// accountId =12334
+
+accountEmail = "vibhanshu@gmail.com"
+accountPassword = "123"
+accountCity = "bareilly"
+console.log(accountEmail);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail, accountId , accountPassword , accountCity])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+let name = "Vibhanshu"
+let age = 24
+let isLoggedin = false 
+
+console.log(typeof null);
+console.log(typeof undefined);
