390705d9446a038d0a7ce5648ec3552f2cf5f220
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-18 16:07:22 +0000

    DateTime

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
