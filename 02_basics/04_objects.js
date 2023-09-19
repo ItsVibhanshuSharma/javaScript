6e2ed3e2a2f9a4b01ba79994deceee30c19e00a7
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-19 16:54:16 +0000

    destructing

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,7 +3,7 @@ const JSUser = {}
 JSUser.name = "Vibhanshu"
 JSUser.number = "766985605"
 
-console.log(JSUser);
+// console.log(JSUser);
 
 const obj1 = {1 : "A",2 :"B",3:"C",4:"D"}
 const obj2 = {5:"E",6:"F",7:"G"}
@@ -14,4 +14,15 @@ const obj2 = {5:"E",6:"F",7:"G"}
 const obj4 = Object.assign({}, obj1 , obj2)
 const obj5 = {...obj1 , ...obj2}
 // console.log(obj4);
-console.log(obj5)
+// console.log(obj5)
+
+
+const course = {
+    courseName : "JavaScript",
+    price : 99,
+    courseInstructor :"Sharma ji "
+
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor);
