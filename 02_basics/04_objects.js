53d64c0b7dc0cdb57f671e6f7b5aa0770302f5a1
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-19 16:40:49 +0000

    Object 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,17 @@
+const JSUser = {}
+ 
+JSUser.name = "Vibhanshu"
+JSUser.number = "766985605"
+
+console.log(JSUser);
+
+const obj1 = {1 : "A",2 :"B",3:"C",4:"D"}
+const obj2 = {5:"E",6:"F",7:"G"}
+
+// console.log(obj1 ,obj2);
+
+// const obj3 = {obj1 , obj2}
+const obj4 = Object.assign({}, obj1 , obj2)
+const obj5 = {...obj1 , ...obj2}
+// console.log(obj4);
+console.log(obj5)
