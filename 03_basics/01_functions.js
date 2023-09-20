cd15d3f3ded33c2237123e7de3d29b6ca428ad79
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-20 16:48:43 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+// function sayMyName(){
+//     console.log("I");
+//     console.log("S");
+//     console.log("H");
+//     console.log("U");
+// }
+
+
+// sayMyName()
+
+function Addition(num1 , num2)
+{
+    const c = num1 + num2
+    return c
+}
+
+
+const a = Addition(2,4)
+console.log(a);
