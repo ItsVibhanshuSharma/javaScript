996d5068a9ad8cd07a6844137d740cb134c1dca1
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-23 05:35:23 +0000

    fucntions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,12 +8,43 @@
 
 // sayMyName()
 
-function Addition(num1 , num2)
-{
-    const c = num1 + num2
-    return c
-}
+// function Addition(num1 , num2)
+// {
+//     const c = num1 + num2
+//     return c
+// }
 
 
-const a = Addition(2,4)
-console.log(a);
+// const a = Addition(2,4)
+// console.log(a);
+
+
+// function one(){
+//     const userName = "Vibanshu"
+
+//       function two(){
+//         const website = "Vibhanshhu.sharma@gmail.com"
+//         console.log(userName);
+//       }
+
+//     //   console.log(website);
+
+//       two()
+// }
+
+// one()
+
+
+if(true)
+ {
+    const userName = "Vibhanshu"
+     if(userName == 'Vibhanshu')
+     {
+        const lastName ="Sharma"
+       console.log(userName + lastName);
+
+     }
+   console.log(lastName);
+    }
+
+    console.log(userName);
