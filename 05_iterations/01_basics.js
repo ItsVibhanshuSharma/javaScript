8862b21eac2ba137c34f85fa59c504951bb48163
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-23 09:39:16 +0000

    loop

---
+++ 05_iterations/01_basics.js
@@ -0,0 +1,33 @@
+// for loop
+
+// for( let i = 0 ; i<= 10 ;i++)
+// {
+//     console.log(i);
+// }
+
+// for(let i =1 ;i <10 ;i++)
+// {
+//      for(let j = 1 ;j<10 ;j++)
+//      {
+//         console.log( i, '* ', j ,'=', i*j);
+//      }
+// }
+
+//Break and continue
+
+// for (let i = 1; i < 21; i++) {
+//   if (i == 5) {
+//     console.log(i);
+//     break;
+//   }
+//  console.log(i);
+// }
+
+
+
+for(let i =0 ;i<21 ;i++)
+{
+    if(i==5)
+    continue;
+console.log(i);
+}
---
+++ 05_iterations/02_basics.js
@@ -0,0 +1,25 @@
+// let i=2;
+// while(i< 20){
+//   console.log(i);
+//   i= i+2
+// }
+
+
+// let myArr = [
+//   'ishu',
+//   'vibhanshu',
+//   'sumit,'
+// ]
+// let arr = 0;
+// while(arr < myArr.length)
+// {
+//     console.log(myArr[arr]);
+//     arr= arr+1
+// }
+
+let score =2
+do{
+    console.log(score);
+    score++;
+
+}while(score <20)
