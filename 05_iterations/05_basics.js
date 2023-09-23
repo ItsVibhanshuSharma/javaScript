fdb05cfab281476ecf6b601552ff3406ad561278
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-23 17:06:19 +0000

    dom

--- 05_iterations/05_basics.js
+++ 05_iterations/05_basics.js
@@ -1,5 +1,16 @@
-const coding = ['c++','java','css','html']
+// const coding = ['c++','java','css','html']
 
-coding.forEach( function (item) {
-   console.log(item);
+// coding.forEach( function (item) {
+//    console.log(item);
+// })
+
+
+const myCoding =[
+    {'i': 'ishu' , 'v':"vibhanshu"},
+    { 'i': "python", 'v':'Java'}
+]
+
+
+myCoding.forEach ( (item) =>{
+    console.log(item.i);
 })
---
+++ 05_iterations/06_basics.js
@@ -0,0 +1,4 @@
+const num = [11,2,34,4,3,43,4,5,1,24,5]
+
+const i = num.filter( (num) =>num >10)
+console.log(i);
