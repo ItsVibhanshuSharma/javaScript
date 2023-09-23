21144846d5a02ef604c6580f6730d164e597e316
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-23 08:32:58 +0000

    switch

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -1,5 +1,40 @@
-const isUserloggedin = false
-if(isUserloggedin)
-{
-    console.log("ishu");
-}
+// const isUserloggedin = false
+// if(2 === "2")
+// {
+//     console.log("ishu");
+// }
+
+// const temp = 41
+
+// if(temp > 40)
+// {
+//     console.log("Greater than");
+// }
+// else{
+//     console.log("Less than");
+// }
+// console.log("Equal ");
+
+// const score = 200
+// if(score > 100)
+// { 
+//     const pwr = "fly"
+//      console.log(`user power ${pwr} `);
+// }
+
+// console.log(`user power ${pwr} `); 
+
+
+const userLoggedin = true
+const debitCard = true
+const userLoggedfromgoogle = true
+const userLoggedfromgmail = false
+
+ if(userLoggedin && debitCard)
+ {
+    console.log('allowed');
+ }
+ if(userLoggedfromgmail || userLoggedfromgoogle)
+ {
+    console.log("Allowed");
+ }
---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,19 @@
+// switch(key)
+// {
+//     case value :
+//         break;
+//         default :
+//          break;
+// }
+const month = 4
+switch (month) {
+  case 1:
+    console.log("1");
+  case 2:
+    console.log("2");
+  case 3:
+    console.log("3");
+  default:
+    console.log("default");
+    break;
+}
