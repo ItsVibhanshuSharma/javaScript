016210b992f91945bdd23acb59fd23fe6f83205f
Vibhanshu Sharma <111836628+ItsVibhanshuSharma@users.noreply.github.com>
2023-09-26 15:52:27 +0000

    myClasses

---
+++ OOP/myClasses.js
@@ -0,0 +1,22 @@
+//ES6
+
+class User{
+    constructor(username ,  email , password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+         return `${this.password}abc`
+    }
+
+    changeUserName (){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User("Ishu" ,"Ishu@gmail.com","123")
+console.log(chai);
+console.log(chai.encryptPassword());
+console.log(chai.changeUserName());
